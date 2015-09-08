46991d300c3991ae66dbc62f46a8419b1ca61643
JeffreyBPetersen <jeffreybpetersen@gmail.com>
2015-09-08 13:03:33 -0700

    complete tutorial application

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var app = require('express')();
 // use express to handle requests made to http server
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var port = 3000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a client connected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('a client disconnected');
+  });
 });
 
 http.listen(3000, function(){
